a82e169701e84d829e49655348d49254b27dba77
GrahamDerry <grahamderry@users.noreply.github.com>
2026-01-30 22:38:43 +0700

    Remove RSVP decorative icon

--- src/components/RSVP.tsx
+++ src/components/RSVP.tsx
@@ -36,18 +36,6 @@ export default function RSVP() {
           </svg>
         </a>
 
-        {/* Decorative element */}
-        <div className="mt-16 opacity-40">
-          <svg
-            className="w-12 h-12 mx-auto text-sage-400"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1"
-          >
-            <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-          </svg>
-        </div>
       </div>
     </section>
   )
